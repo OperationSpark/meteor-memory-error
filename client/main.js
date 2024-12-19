210e3b90e3d26ddd5c33ecd3bfe6d229d9ce0603
Harvey Sanders <sanders.harvey@gmail.com>
2024-12-19 14:44:03 -0500

    Swap Blaze for React

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import './hello.js';
+
 
 Template.hello.onCreated(function helloOnCreated() {
   this.counter = new ReactiveVar(0);
